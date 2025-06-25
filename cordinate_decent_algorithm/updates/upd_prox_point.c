de852e963ee34d4fec5eaab7d3991dd2d3e08c15
OganKuba <kubog123@gmail.com>
2025-06-25 09:48:05 +0200

    final 2

--- cordinate_decent_algorithm/updates/upd_prox_point.c
+++ cordinate_decent_algorithm/updates/upd_prox_point.c
@@ -14,27 +14,22 @@
  */
 static void prox_point_update(CDState* st, int j)
 {
-    int m = st->m;
+    int m  = st->m;
     const double* Xj = st->X + (size_t)j * m;
 
-    // Gradient: ∇_j = -X_jᵀ r
     double grad_j = -dot(Xj, st->resid, m);
-    double L_j = st->norm2[j];
-    double alpha = 1.0 / L_j;
+    double L_j    = st->norm2[j];
 
-    // Proximal step
-    double prox_center = st->beta[j] - alpha * grad_j;
-    double new_beta = shrink(prox_center, st->lam * alpha);
+    double A  = 2.0 * L_j;
 
-    double delta = new_beta - st->beta[j];
-    if (delta == 0.0)
-        return;
+    double z         = st->beta[j] - grad_j / A;
+    double new_beta  = shrink(z, st->lam / A);
+    double delta     = new_beta - st->beta[j];
+    if (delta == 0.0) return;
 
-    // Update β and residual
     st->beta[j] = new_beta;
     axpy(-delta, Xj, st->resid, m);
 
-    // Optionally store gradient
     if (st->grad)
         st->grad[j] = grad_j;
 }
